cd4c75fe62e3906859580bc45de374ce2e58fd3f
rountable02 <wontac02@gmail.com>
2025-06-10 00:50:34 +0900

    Add ./src/main/java/neetcode/boj/BOJ1000.java

---
+++ src/main/java/neetcode/boj/BOJ1000.java
@@ -0,0 +1,15 @@
+package neetcode.boj;
+import java.util.Scanner;
+
+public class BOJ1000 {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+
+        sc.close();
+    }
+}
