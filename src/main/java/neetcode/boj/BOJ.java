2e0e7c2c2b913c36238259b12af9f99d654d0cd7
rountable02 <wontac02@gmail.com>
2025-06-13 17:11:35 +0900

    Add ./src/main/java/neetcode/boj/BOJ.java

---
+++ src/main/java/neetcode/boj/BOJ.java
@@ -0,0 +1,11 @@
+package neetcode.boj;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ {
+    public static void main(String[] args) throws IOException {
+//        Queue<Integer> queue = new LinkedList<>();
+
+    }
+}
