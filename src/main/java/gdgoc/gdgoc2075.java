7365345339f5476415f4dc5e6994c351999a7f93
rountable02 <wontac02@gmail.com>
2025-10-30 23:17:20 +0900

    20251030

---
+++ src/main/java/gdgoc/gdgoc2075.java
@@ -0,0 +1,25 @@
+package gdgoc;
+
+import java.io.*;
+import java.util.*;
+
+public class gdgoc2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                pq.offer(num);
+                if (pq.size() > N) {
+                    pq.poll();
+                }
+            }
+        }
+        System.out.println(pq.peek());
+    }
+}
+
