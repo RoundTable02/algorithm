73083a0e20652f119e174a98cdce0d3c0cffea66
rountable02 <wontac02@gmail.com>
2025-11-15 21:19:30 +0900

    20251115

---
+++ src/main/java/gdgoc/gdgoc2467.java
@@ -0,0 +1,41 @@
+package gdgoc;
+
+import java.util.*;
+import java.io.*;
+
+public class gdgoc2467 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0, right = N - 1;
+        int best = Integer.MAX_VALUE;
+        int ansL = 0; int ansR = 0;
+
+        while (left < right) {
+            int hap = arr[left] + arr[right];
+            if (best > Math.abs(hap)) {
+                best = Math.abs(hap);
+                ansL = arr[left];
+                ansR = arr[right];
+            }
+
+            if (hap > 0) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+
+        System.out.println(ansL + " " + ansR);
+    }
+}
