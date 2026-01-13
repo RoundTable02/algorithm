704e56fc1312fd0c20d5c9b8370a96e3e8f4b674
rountable02 <wontac02@gmail.com>
2026-01-13 23:13:43 +0900

    20260113

---
+++ src/main/java/bojclass/Main11054.java
@@ -0,0 +1,45 @@
+package bojclass;
+
+import java.io.*;
+import java.util.*;
+
+public class Main11054 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] lis = new int[n];
+        int[] lds = new int[n];
+
+        // LIS
+        for (int i = 0; i < n; i++) {
+            lis[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    lis[i] = Math.max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+
+        // LDS (뒤에서부터)
+        for (int i = n - 1; i >= 0; i--) {
+            lds[i] = 1;
+            for (int j = n - 1; j > i; j--) {
+                if (arr[j] < arr[i]) {
+                    lds[i] = Math.max(lds[i], lds[j] + 1);
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, lis[i] + lds[i] - 1);
+        }
+
+        System.out.println(ans);
+    }
+}
